8529387a8c5ddb5e2dae10b0bd92d2b0598edbb0
taojoue <taojouet@gmail.com>
2022-06-29 13:55:21 +0200

    Urls

--- index.js
+++ index.js
@@ -29,7 +29,18 @@ async function scrapper() {
     let result = [];
     let driver = await new Builder().forBrowser('firefox').build();
 
-
+    const urls = [
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/linda-mccartney-and-paul-ram-1062783",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/the-go-gos-beauty-and-the-beat-1062833",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/stevie-wonder-music-of-my-mind-2-1062883",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/shania-twain-come-on-over-1062933",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/buzzcocks-singles-going-steady-2-1062983",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/sade-diamond-life-1063033",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/bruce-springsteen-nebraska-3-1063083",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/the-band-music-from-big-pink-2-1063133",
+        "https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/jay-z-the-blueprint-3-1063183"
+    ];
 
 
     // mongoose.connect(process.env.MONGO_URL,
@@ -49,80 +60,83 @@ async function scrapper() {
     // // Explicitly create the collection before using it
     // // so the collection is capped.
     // await Model.createCollection();
+    for (let n = 0; n < urls.length; n++) {
+        try {
+            // Navigate to Url
+            console.log(n)
+            await driver.get(urls[n]);
 
-    try {
-        // Navigate to Url
-        await driver.get('https://www.rollingstone.com/music/music-lists/best-albums-of-all-time-1062063/');
+            let id = await driver.findElements(By.className('c-gallery-vertical-album__number'));
+            const nbOfElements = id.length;
+            let title = await driver.findElements(By.className('c-gallery-vertical-album__title'));
+            let subtitle = await driver.findElements(By.className('c-gallery-vertical-album__subtitle'));
+            let description = await driver.findElements(By.className('c-gallery-vertical-album__description'));
+            let image = await driver.findElements(By.className('c-gallery-vertical-album__image u-gallery-react-placeholder-shimmer'));
+            // let image = await driver.findElements(By.className('c-gallery-vertical-album__image'));
 
-        let id = await driver.findElements(By.className('c-gallery-vertical-album__number'));
-        const nbOfElements = id.length;
-        let title = await driver.findElements(By.className('c-gallery-vertical-album__title'));
-        let subtitle = await driver.findElements(By.className('c-gallery-vertical-album__subtitle'));
-        let description = await driver.findElements(By.className('c-gallery-vertical-album__description'));
-        let image = await driver.findElements(By.className('c-gallery-vertical-album__image u-gallery-react-placeholder-shimmer'));
-        // let image = await driver.findElements(By.className('c-gallery-vertical-album__image'));
+            // curl https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-500-Arcade-Fire-Funeral.jpg?w=800 -o ./Images/test.png
+            // console.log(id);
+            // for (let i of id) {
+            // result.push({ id: await i.getText() });
+            for (let n = 0; n < nbOfElements; n++) {
+                let i = await id[n].getText();
+                let t = await title[n].getText();
+                let s = await subtitle[n].getText();
+                let d = await description[n].getText();
 
-        // curl https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-500-Arcade-Fire-Funeral.jpg?w=800 -o ./Images/test.png
-        // console.log(id);
-        // for (let i of id) {
-        // result.push({ id: await i.getText() });
-        for (let n = 0; n < nbOfElements; n++) {
-            let i = await id[n].getText();
-            let t = await title[n].getText();
-            let s = await subtitle[n].getText();
-            let d = await description[n].getText();
-
-            let tURL = t.replace(/'/g, "").replace(/’/g, "").replace(/,/g, "").replace(/!/g, "").replace("+", "").replace(/é/g, "").replace(/ /g, "-");
-            tURL = tURL.replace("--", "x").replace("/", "-");
-            let url = "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-" + i + "-" + tURL + ".jpg"
-            // console.log(url);
-            result.push({
-                id: i,
-                title: t,
-                subtitle: s,
-                url: url,
-                // description: d,
-            });
-            try {
-                exec("curl " + url + " -o ./Images/" + i + ".jpg");
-            } catch (error) {
-                console.log(error);
+                let tURL = t.replace(/'/g, "").replace(/’/g, "").replace(/,/g, "").replace(/!/g, "").replace("+", "").replace(/é/g, "e").replace(/ /g, "-");
+                tURL = tURL.replace("--", "x").replace("/", "-");
+                let url = "https://www.rollingstone.com/wp-content/uploads/2020/09/R1344-" + i + "-" + tURL + ".jpg"
+                // console.log(url);
+                result.push({
+                    id: i,
+                    title: t,
+                    subtitle: s,
+                    url: url,
+                    // description: d,
+                });
+                try {
+                    exec("curl " + url + " -o ./Images/"  + i + ".jpg");
+                } catch (error) {
+                    console.log(error);
+                }
             }
+            for (let t of title) {
+                // result.push({ title: await t.getText() });
+
+                // console.log(await im.getAttribute("src"));
+                // app.post('/', upload.single('image'), (req, res, next) => {
+
+                // let obj = {
+                //     id: id,
+                //     title: title,
+                //     subtitle: subtitle,
+                //     description: description,
+                //     // img: {
+                //     //     data: fs.readFileSync(path.join(image)),
+                //     //     contentType: 'image/png'
+                //     // }
+                // }
+
+                // imgModel.create(obj, (err, item) => {
+                //     if (err) {
+                //         console.log(err);
+                //     }
+                //     else {
+                //         // item.save();
+                //         res.redirect('/');
+                //     }
+                // });
+                // });
+            }
+
         }
-        for (let t of title) {
-            // result.push({ title: await t.getText() });
-
-            // console.log(await im.getAttribute("src"));
-            // app.post('/', upload.single('image'), (req, res, next) => {
-
-            // let obj = {
-            //     id: id,
-            //     title: title,
-            //     subtitle: subtitle,
-            //     description: description,
-            //     // img: {
-            //     //     data: fs.readFileSync(path.join(image)),
-            //     //     contentType: 'image/png'
-            //     // }
-            // }
-
-            // imgModel.create(obj, (err, item) => {
-            //     if (err) {
-            //         console.log(err);
-            //     }
-            //     else {
-            //         // item.save();
-            //         res.redirect('/');
-            //     }
-            // });
-            // });
+        finally {
+            await driver.quit();
         }
-
+        // console.log(result);
+        // });
     }
-    finally {
-        await driver.quit();
-    }
-    // console.log(result);
     return;
 }
 module.exports = scrapper();
